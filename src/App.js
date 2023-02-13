abec018f5734ab98473e3c985b530cfe3922cd94
payton <45017406+paytontaylor@users.noreply.github.com>
2023-02-13 11:17:48 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -39,49 +39,4 @@ const App = () => {
     )
 }
 
-
-// class App extends Component {
-//     constructor(){
-//         super();
-
-//         this.state = {
-//             monsters: [],
-//             searchField: '',
-//         }
-//     }
-
-//     componentDidMount() {
-//         fetch("https://jsonplaceholder.typicode.com/users")
-//             .then(resp => resp.json())
-//             .then(users => this.setState(() => {
-//                 return { monsters: users}
-//             }));
-//         };
-
-//     onSearchChange = (evt) => {
-//         const searchField = evt.target.value.toLocaleLowerCase();
-//         this.setState(() => {
-//         return { searchField }
-//         })
-//     };
-
-//     render() {
-//             const { monsters, searchField } = this.state;
-//             const { onSearchChange } = this;
-    
-//             const filteredMonsters = monsters.filter((monster) => {
-//                 return monster.name.toLowerCase().includes(searchField)
-//             });
-
-//         return (         
-//             <div className="App">
-//                 <h1 className="app-title">Monsters Rolodex</h1>
- 
-//                 <SearchBox onChangeHandler={onSearchChange} className="search-box" placeholder="search monsters"/>
-//                 <CardList monsters={filteredMonsters} />
-//             </div>
-//         );
-//     }
-// }
-
 export default App;
